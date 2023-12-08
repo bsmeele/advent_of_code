7432fb43c510200f548138f8af928ff3a912d4e9
bsmeele <bas.smeele2000@gmail.com>
2023-12-08 13:53:39 +0100

    day 8 part 2

--- src/y2022/day22/mod.rs
+++ src/y2022/day22/mod.rs
@@ -1,13 +1,97 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 pub fn monkey_map() {
-    let test = false;
+    let test = true;
     let filename = if test { "src/y2022/day22/test" } else { "src/y2022/day22/input" };
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
-    for _line in reader.lines().map(|x| x.unwrap()) {
+    let mut path: Vec<char> = Vec::new();
+    let mut map: HashMap<(usize, usize), bool> = HashMap::new();
+    let mut x = 0;
+    let mut y = 0;
 
+    for line in reader.lines().map(|x| x.unwrap()) {
+        x = 0;
+        let line = line.chars().collect::<Vec<char>>();
+        if line.is_empty() { continue; }
+        if line[0].is_numeric() || line[0] == 'L' || line[0] == 'R' {
+            path = line.clone();
+        }
+        if map.is_empty() { y = 0; }
+        for c in line {
+            match c {
+                '.' => _ = map.insert((x, y), false),
+                '#' => _ = map.insert((x, y), true),
+                _ => (),
+            }
+            x += 1;
+        }
+        y += 1;
     }
+
+    let mut start = (0,0);
+    let mut x_max = 0;
+    let mut y_max = 0;
+    for ((x, y), _) in &map {
+        if *x > x_max { x_max = *x; }
+        if *y > y_max { y_max = *y; }
+    }
+    'find_start: loop {
+        for y in 0..=y_max {
+            for x in 0..=x_max {
+                match map.get(&(x, y)) {
+                    Some(b) if !*b => {
+                        start = (x, y);
+                        break 'find_start;
+                    }
+                    _ => continue,
+                }
+            }
+        }
+    }
+
+    // draw_map(&map);
+
+    let mut facing = Facing::Right;
+    let mut loc = start;
+
+    todo!()
+
+    let result = 1000 * (loc.1 + 1) + 4 * (loc.0 + 1) + match facing {
+        Facing::Right => 0,
+        Facing::Down => 1,
+        Facing::Left => 2,
+        Facing::Up => 3,
+    };
+    println!("year 2022 day 22 part 1: {}", result);
+}
+
+enum Facing {
+    Right,
+    Down,
+    Left,
+    Up,
+}
+
+#[allow(dead_code)]
+fn draw_map(map: &HashMap<(usize, usize), bool>) {
+    let mut x_max = 0;
+    let mut y_max = 0;
+    for ((x, y), _) in map {
+        if *x > x_max { x_max = *x; }
+        if *y > y_max { y_max = *y; }
+    }
+    for y in 0..=y_max {
+        for x in 0..=x_max {
+            match map.get(&(x, y)) {
+                Some(b) => if *b { print!("#") } else { print!(".") },
+                None => print!(" "),
+            }
+        }
+        println!();
+    }
+    println!();
 }
--- src/y2023/day8/mod.rs
+++ src/y2023/day8/mod.rs
@@ -1,9 +1,11 @@
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::mem::swap;
 
 pub fn haunted_wasteland() {
     let test = false;
+    let verbose = false;
     let mut filename = if test { "src/y2023/day8/test1" } else { "src/y2023/day8/input" };
     let mut file = File::open(filename).unwrap();
     let mut reader = BufReader::new(file);
@@ -50,6 +52,7 @@ pub fn haunted_wasteland() {
     network.clear();
     ins_parsed = false;
     let mut current_nodes: Vec<String> = Vec::new();
+    let mut end_nodes: Vec<String> = Vec::new();
 
     if test {
         filename = "src/y2023/day8/test2";
@@ -71,41 +74,134 @@ pub fn haunted_wasteland() {
                 if node.chars().collect::<Vec<char>>().last().unwrap() == &'A' {
                     current_nodes.push(String::from(node));
                 }
+                if node.chars().collect::<Vec<char>>().last().unwrap() == &'Z' {
+                    end_nodes.push(String::from(node));
+                }
                 network.insert(String::from(node), (l, r));
             }
         }
     }
 
-    id = 0;
-    steps = 0;
-    let mut finished;
-    loop {
-        finished = true;
-        let mut z_count = 0;
-        for n in &current_nodes {
-            if n.chars().collect::<Vec<char>>().last().unwrap() != &'Z' {
-                finished = false;
-                break;
-            } else {
-                z_count += 1;
-            }
-        }
-        // if z_count > 2 {
-        //     println!("{} {:?}", steps, current_nodes);
-        // }
-        if finished { break; }
+    // Notes:
+    // - from any start to some end takes a multiple of the instruction length amount of steps
+    // - from any end to some end takes a multiple of the instruction length amount of steps
+    // - from any start to some end takes the same amount of steps as from that end to some end
+    // - Concluding: the solution is the least common multiple of each individual start to end
+
+    if verbose {
         for i in 0..current_nodes.len() {
-            let (l, r) = &network[&current_nodes[i]];
-            match instructions[id] {
-                'L' => current_nodes[i] = l.clone(),
-                'R' => current_nodes[i] = r.clone(),
-                _ => panic!("Unreachable")
+            id = 0;
+            steps = 0;
+            current_node = &current_nodes[i];
+            loop {
+                if current_node.chars().collect::<Vec<char>>().last().unwrap() == &'Z' {
+                    break;
+                }
+                let (l, r) = &network[current_node];
+                match instructions[id] {
+                    'L' => current_node = l,
+                    'R' => current_node = r,
+                    _ => panic!("Unreachable")
+                }
+                id += 1;
+                if id == instructions.len() { id = 0; }
+                steps += 1;
             }
+            println!("Start: {}, end: {}, number of steps: {}, instruction number: {}", current_nodes[i], current_node, steps, id);
         }
-        id += 1;
-        if id == instructions.len() { id = 0; }
-        steps += 1;
+
+        for i in 0..end_nodes.len() {
+            id = 0;
+            steps = 0;
+            current_node = &current_nodes[i];
+            loop {
+                let (l, r) = &network[current_node];
+                match instructions[id] {
+                    'L' => current_node = l,
+                    'R' => current_node = r,
+                    _ => panic!("Unreachable")
+                }
+                id += 1;
+                if id == instructions.len() { id = 0; }
+                steps += 1;
+
+                if current_node.chars().collect::<Vec<char>>().last().unwrap() == &'Z' {
+                    break;
+                }
+            }
+            println!("Start: {}, end: {}, number of steps: {}, instruction number: {}", end_nodes[i], current_node, steps, id);
+        }
     }
 
-    println!("Year 2023 day 8 part 2: {}", steps);
+    let mut steps_list: Vec<usize> = Vec::new();
+    for node in &current_nodes {
+        id = 0;
+        steps = 0;
+        current_node = node;
+        loop {
+            if current_node.chars().collect::<Vec<char>>().last().unwrap() == &'Z' { break; }
+            let (l, r) = &network[current_node];
+            match instructions[id] {
+                'L' => current_node = l,
+                'R' => current_node = r,
+                _ => panic!("Unreachable")
+            }
+
+            id += 1;
+            if id == instructions.len() { id = 0; }
+            steps += 1;
+        }
+        steps_list.push(steps);
+    }
+
+    let mut num = 1;
+    for n in &steps_list {
+        num = lcm(num, *n);
+    }
+
+    // ----- Brute force approach -----
+    // id = 0;
+    // steps = 0;
+    // let mut finished;
+    // loop {
+    //     finished = true;
+    //     for n in &current_nodes {
+    //         if n.chars().collect::<Vec<char>>().last().unwrap() != &'Z' {
+    //             finished = false;
+    //             break;
+    //         }
+    //     }
+    //     if finished { break; }
+    //     for i in 0..current_nodes.len() {
+    //         let (l, r) = &network[&current_nodes[i]];
+    //         match instructions[id] {
+    //             'L' => current_nodes[i] = l.clone(),
+    //             'R' => current_nodes[i] = r.clone(),
+    //             _ => panic!("Unreachable")
+    //         }
+    //     }
+    //     id += 1;
+    //     if id == instructions.len() { id = 0; }
+    //     steps += 1;
+    // }
+
+    println!("Year 2023 day 8 part 2: {}", num);
+}
+
+fn lcm(a: usize, b: usize) -> usize {
+    a * b / gcd(a, b)
+}
+
+fn gcd(a: usize, b: usize) -> usize {
+    let mut a = a;
+    let mut b = b;
+    if a < b {
+        swap(&mut a, &mut b);
+    }
+    while b != 0 {
+        let r = a % b;
+        a = b;
+        b = r;
+    }
+    a
 }
