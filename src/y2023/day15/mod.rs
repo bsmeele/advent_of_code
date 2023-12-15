a7dcb437e60962443e5eb816e13d1fa95f5fc27e
bsmeele <bas.smeele2000@gmail.com>
2023-12-15 12:20:07 +0100

    ugh

--- src/y2023/day15/mod.rs
+++ src/y2023/day15/mod.rs
@@ -1,18 +1,35 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+const EMPTY_VEC: Vec<(&str, usize)> = Vec::new();
+
 pub fn lens_library() {
-    let test = false;
+    let test = true;
     let filename = if test { "src/y2023/day15/test" } else { "src/y2023/day15/input" };
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
     let mut acc = 0;
+    let mut boxes: [Vec<(&str, usize)>; 256] = [EMPTY_VEC; 256];
+
     for line in reader.lines().map(|x| x.unwrap()) {
         let line = line.split(',').collect::<Vec<&str>>();
         for s in line {
             let tmp = hash(s);
             acc += tmp;
+
+            let mut label = String::new();
+            for c in s.chars() {
+                match c {
+                    '=' => {
+                        let lens = s.chars().last().unwrap().to_digit(10).unwrap();
+                        let b = hash(&label);
+                        println!("{}={}", label, lens)
+                    },
+                    '-' => println!("{}-", label),
+                    _ => label.push(c),
+                }
+            }
         }
     }
 
