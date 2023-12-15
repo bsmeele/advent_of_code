7b79f1270590832ca1b549b156d67a8fe1f88d0a
bsmeele <bas.smeele2000@gmail.com>
2023-12-15 08:22:23 +0100

    day15 part1

--- src/y2023/day15/mod.rs
+++ src/y2023/day15/mod.rs
@@ -7,7 +7,24 @@ pub fn lens_library() {
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
+    let mut acc = 0;
     for line in reader.lines().map(|x| x.unwrap()) {
-
+        let line = line.split(',').collect::<Vec<&str>>();
+        for s in line {
+            let tmp = hash(s);
+            acc += tmp;
+        }
     }
+
+    println!("Year 2023 day 15 part 1: {}", acc);
+}
+
+fn hash(s: &str) -> usize {
+    let mut current_value = 0;
+    for c in s.chars() {
+        current_value += (c as u8) as usize;
+        current_value *= 17;
+        current_value %= 256;
+    }
+    current_value
 }
