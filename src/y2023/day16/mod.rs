03cbd346fd90badbc8cbefba983535f6f00d24ce
bsmeele <bas.smeele2000@gmail.com>
2023-12-16 12:07:20 +0100

    day16 improvement

--- src/y2023/day16/mod.rs
+++ src/y2023/day16/mod.rs
@@ -13,16 +13,10 @@ pub fn the_floor_will_be_lava() {
     for line in reader.lines().map(|x| x.unwrap()) {
         map.push(line.chars().collect())
     }
-
     // draw_map(&map);
 
-    let direction = match map[0][0] {
-        '/' => Direction::North,
-        '\\' | '|' => Direction::South,
-        _ => Direction::East,
-    };
     let mut map_part1 = map.clone();
-    let tiles = simulate_beam(&mut map_part1, (0, 0), direction, HashSet::new());
+    let tiles = simulate_beam(&mut map_part1, (0, 0), Direction::East, HashSet::new());
     // draw_map(&map_part1);
 
     println!("Year 2023 day 16 part 1: {}", tiles.len());
@@ -33,24 +27,13 @@ pub fn the_floor_will_be_lava() {
     let mut max_map = map.clone();
     for x in 0..map[0].len() {
         map_part2 = map.clone();
-        res = match map[0][x] {
-            '/' => simulate_beam(&mut map_part2, (x, 0), Direction::West, HashSet::new()).len(),
-            '\\' => simulate_beam(&mut map_part2, (x, 0), Direction::East, HashSet::new()).len(),
-            '-' => simulate_beam(&mut map_part2, (x, 0), Direction::West, HashSet::new()).len() + simulate_beam(&mut map_part2, (x, 0), Direction::East, HashSet::new()).len(),
-            _ => simulate_beam(&mut map_part2, (x, 0), Direction::South, HashSet::new()).len(),
-        };
+        res = simulate_beam(&mut map_part2, (x, 0), Direction::South, HashSet::new()).len();
         if res > max_energized {
             max_energized = res;
             max_map = map_part2.clone();
         }
-
         map_part2 = map.clone();
-        res = match map[map.len()-1][x] {
-            '/' => simulate_beam(&mut map_part2, (x, map.len()-1), Direction::East, HashSet::new()).len(),
-            '\\' => simulate_beam(&mut map_part2, (x, map.len()-1), Direction::West, HashSet::new()).len(),
-            '-' => simulate_beam(&mut map_part2, (x, map.len()-1), Direction::East, HashSet::new()).len() + simulate_beam(&mut map_part2, (x, 0), Direction::West, HashSet::new()).len(),
-            _ => simulate_beam(&mut map_part2, (x, map.len()-1), Direction::North, HashSet::new()).len(),
-        };
+        res = simulate_beam(&mut map_part2, (x, map.len()-1), Direction::North, HashSet::new()).len();
         if res > max_energized {
             max_energized = res;
             max_map = map_part2.clone();
@@ -59,31 +42,20 @@ pub fn the_floor_will_be_lava() {
 
     for y in 0..map.len() {
         map_part2 = map.clone();
-        res = match map[y][0] {
-            '/' => simulate_beam(&mut map_part2, (0, y), Direction::North, HashSet::new()).len(),
-            '\\' => simulate_beam(&mut map_part2, (0, y), Direction::South, HashSet::new()).len(),
-            '-' => simulate_beam(&mut map_part2, (0, y), Direction::North, HashSet::new()).len() + simulate_beam(&mut map_part2, (0, y), Direction::South, HashSet::new()).len(),
-            _ => simulate_beam(&mut map_part2, (0, y), Direction::East, HashSet::new()).len(),
-        };
+        res = simulate_beam(&mut map_part2, (0, y), Direction::East, HashSet::new()).len();
         if res > max_energized {
             max_energized = res;
             max_map = map_part2.clone();
         }
-
         map_part2 = map.clone();
-        res = match map[y][map[0].len()-1] {
-            '/' => simulate_beam(&mut map_part2, (map[0].len()-1, y), Direction::South, HashSet::new()).len(),
-            '\\' => simulate_beam(&mut map_part2, (map[0].len()-1, y), Direction::North, HashSet::new()).len(),
-            '-' => simulate_beam(&mut map_part2, (map[0].len()-1, y), Direction::South, HashSet::new()).len() + simulate_beam(&mut map_part2, (map[0].len()-1, y), Direction::North, HashSet::new()).len(),
-            _ => simulate_beam(&mut map_part2, (map[0].len()-1, y), Direction::West, HashSet::new()).len(),
-        };
+        res = simulate_beam(&mut map_part2, (map[0].len()-1, y), Direction::West, HashSet::new()).len();
         if res > max_energized {
             max_energized = res;
             max_map = map_part2.clone();
         }
     }
-
     // draw_map(&max_map);
+
     println!("Year 2023 day 16 part 2: {}", max_energized);
 }
 
@@ -120,67 +92,66 @@ fn simulate_beam(map: &mut Vec<Vec<char>>, start: (usize, usize), direction: Dir
 
         match direction {
             Direction::North => {
-                if loc.1 <= 0 { break; }
-                loc.1 -= 1;
                 match map[loc.1][loc.0] {
                     '\\' => direction = Direction::West,
                     '/' => direction = Direction::East,
                     '-' => {
-                        tiles.extend(simulate_beam(map, loc, Direction::West, tiles.clone()));
-                        tiles.extend(simulate_beam(map, loc, Direction::East, tiles.clone()));
+                        if loc.0 > 0 { tiles.extend(simulate_beam(map, (loc.0 - 1, loc.1), Direction::West, tiles.clone())); }
+                        if loc.0 + 1 < map[0].len() { tiles.extend(simulate_beam(map, loc, Direction::East, tiles.clone())); }
                         break;
                     },
                     _ => (),
                 }
             },
             Direction::East => {
-                if loc.0 + 1 >= map[0].len() { break; }
-                loc.0 += 1;
                 match map[loc.1][loc.0] {
                     '\\' => direction = Direction::South,
                     '/' => direction = Direction::North,
                     '|' => {
-                        tiles.extend(simulate_beam(map, loc, Direction::North, tiles.clone()));
-                        tiles.extend(simulate_beam(map, loc, Direction::South, tiles.clone()));
+                        if loc.1 > 0 { tiles.extend(simulate_beam(map, (loc.0, loc.1 - 1), Direction::North, tiles.clone())); }
+                        if loc.1 + 1 < map.len() { tiles.extend(simulate_beam(map, (loc.0, loc.1 + 1), Direction::South, tiles.clone())); }
                         break;
                     },
                     _ => (),
                 }
             },
             Direction::South => {
-                if loc.1 + 1 >= map.len() { break; }
-                loc.1 += 1;
                 match map[loc.1][loc.0] {
                     '\\' => direction = Direction::East,
                     '/' => direction = Direction::West,
                     '-' => {
-                        tiles.extend(simulate_beam(map, loc, Direction::West, tiles.clone()));
-                        tiles.extend(simulate_beam(map, loc, Direction::East, tiles.clone()));
+                        if loc.0 > 0 { tiles.extend(simulate_beam(map, (loc.0 - 1, loc.1), Direction::West, tiles.clone())); }
+                        if loc.0 + 1 < map[0].len() { tiles.extend(simulate_beam(map, loc, Direction::East, tiles.clone())); }
                         break;
                     }
                     _ => (),
                 }
             },
             Direction::West => {
-                if loc.0 <= 0 { break; }
-                loc.0 -= 1;
                 match map[loc.1][loc.0] {
                     '\\' => direction = Direction::North,
                     '/' => direction = Direction::South,
                     '|' => {
-                        tiles.extend(simulate_beam(map, loc, Direction::North, tiles.clone()));
-                        tiles.extend(simulate_beam(map, loc, Direction::South, tiles.clone()));
+                        if loc.1 > 0 { tiles.extend(simulate_beam(map, (loc.0, loc.1 - 1), Direction::North, tiles.clone())); }
+                        if loc.1 + 1 < map.len() { tiles.extend(simulate_beam(map, (loc.0, loc.1 + 1), Direction::South, tiles.clone())); }
                         break;
                     },
                     _ => (),
                 }
             },
         }
+        match direction {
+            Direction::North => if loc.1 > 0 { loc.1 -= 1; } else { break; },
+            Direction::East => if loc.0 + 1 < map[0].len() { loc.0 += 1; } else { break; },
+            Direction::South => if loc.1 + 1 < map.len() { loc.1 +=1; } else { break; },
+            Direction::West => if loc.0 > 0 { loc.0 -= 1; } else { break; },
+                    }
     }
 
     tiles
 }
 
+#[allow(dead_code)]
 fn draw_map(map: &Vec<Vec<char>>) {
     for r in map {
         for c in r {
