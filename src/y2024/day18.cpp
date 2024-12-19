90637aa60a2b4ea786b78dda436f287f97719775
bsmeele <bas.smeele2000@gmail.com>
2024-12-19 09:57:31 +0100

    day19

--- src/y2024/day18.cpp
+++ src/y2024/day18.cpp
@@ -3,6 +3,78 @@
 #include <string>
 #include <sstream>
 #include <cstdint>
+#include <vector>
+#include <regex>
+#include <utility>
+#include <unordered_set>
+
+struct PairHash {
+    template <typename T1, typename T2>
+    std::size_t operator()(const std::pair<T1, T2>& p) const {
+        auto h1 = std::hash<T1>()(p.first);
+        auto h2 = std::hash<T2>()(p.second);
+        return h1 ^ (h2 << 1);
+    }
+};
+
+void draw_map(std::unordered_set<std::pair<uint32_t, uint32_t>, PairHash> map, std::pair<uint32_t, uint32_t> map_size, std::unordered_set<std::pair<uint32_t, uint32_t>, PairHash> path) {
+    for (int y = 0; y < map_size.second; y++) {
+        for (int x = 0; x < map_size.first; x++) {
+            if (map.count(std::pair(x, y)) != 0) { std::cout << '#'; }
+            else if (path.count(std::pair(x, y)) != 0) { std::cout << 'O'; }
+            else { std::cout << '.'; }
+        }
+        std::cout << std::endl;
+    }
+}
+
+std::unordered_set<std::pair<uint32_t, uint32_t>, PairHash> find_exit(std::unordered_set<std::pair<uint32_t, uint32_t>, PairHash> map, std::pair<uint32_t, uint32_t> map_size) {
+    std::vector<std::pair<uint32_t, uint32_t>> stack;
+    std::vector<std::vector<uint32_t>> dist(map_size.second, std::vector<uint32_t>(map_size.first, UINT32_MAX));
+    stack.push_back(std::pair(0, 0));
+    dist[0][0] = 0;
+    while (stack.size() > 0) {
+        auto [x, y] = stack.back();
+        stack.pop_back();
+
+        if (x == map_size.first-1 && y == map_size.second-1) { continue; }
+
+        if (y > 0 && map.count(std::pair(x, y-1)) == 0 && dist[y-1][x] > dist[y][x] + 1) {
+            stack.push_back(std::pair(x, y-1));
+            dist[y-1][x] = dist[y][x] + 1;
+        }
+        if (x < map_size.second-1 && map.count(std::pair(x+1, y)) == 0 && dist[y][x+1] > dist[y][x] + 1) {
+            stack.push_back(std::pair(x+1, y));
+            dist[y][x+1] = dist[y][x] + 1;
+        }
+        if (y < map_size.second-1 && map.count(std::pair(x, y+1)) == 0 && dist[y+1][x] > dist[y][x] + 1) {
+            stack.push_back(std::pair(x, y+1));
+            dist[y+1][x] = dist[y][x] + 1;
+        }
+        if (x > 0 && map.count(std::pair(x-1, y)) == 0 && dist[y][x-1] > dist[y][x] + 1) {
+            stack.push_back(std::pair(x-1, y));
+            dist[y][x-1] = dist[y][x] + 1;
+        }
+    }
+
+
+    std::unordered_set<std::pair<uint32_t, uint32_t>, PairHash> path;
+    if (dist[map_size.second-1][map_size.first-1] == UINT32_MAX) { return path; }
+    std::pair<uint32_t, uint32_t> loc = std::pair(map_size.first-1, map_size.second-1);
+
+    while (true) {
+        path.insert(loc);
+
+        if (loc.first == 0 && loc.second == 0) { break; }
+
+        if (loc.second > 0 && dist[loc.second-1][loc.first] == dist[loc.second][loc.first] - 1) { loc = std::pair(loc.first, loc.second-1); }
+        else if (loc.first < map_size.first-1 && dist[loc.second][loc.first+1] == dist[loc.second][loc.first] - 1) { loc = std::pair(loc.first+1, loc.second); }
+        else if (loc.second < map_size.second-1 && dist[loc.second+1][loc.first] == dist[loc.second][loc.first] - 1) { loc = std::pair(loc.first, loc.second+1); }
+        else if (loc.first > 0 && dist[loc.second][loc.first-1] == dist[loc.second][loc.first] - 1) { loc = std::pair(loc.first-1, loc.second); }
+    }
+
+    return path;
+}
 
 void day18(bool test) {
     std::string filepath;
@@ -15,9 +87,48 @@ void day18(bool test) {
         return;
     }
 
+    std::pair<uint32_t, uint32_t> map_size;
+    if (test) { map_size = std::pair(7, 7); }
+    else { map_size = std::pair(71, 71); }
+
+    std::regex pattern(R"((\d+),(\d+))");
+    std::smatch matches;
+
+    std::vector<std::pair<uint32_t, uint32_t>> bytes;
+
     std::string line;
     while (std::getline(input, line)) {
-        std::cout << line << std::endl;
+        if (std::regex_match(line, matches, pattern)) {
+            uint32_t x = std::stoul(matches[1].str());
+            uint32_t y = std::stoul(matches[2].str());
+            bytes.push_back(std::pair(x, y));
+        }
     }
-    std::cout << "Year 2024 day 18 not implemented yet" << std::endl;
+     
+    std::unordered_set<std::pair<uint32_t, uint32_t>, PairHash> map;
+    int lim;
+    if (test) { lim = 12; }
+    else { lim = 1024; }
+    for (int i = 0; i < lim; i++) {
+        map.insert(bytes[i]);
+    }
+
+    std::unordered_set<std::pair<uint32_t, uint32_t>, PairHash> path = find_exit(map, map_size);
+
+    std::cout << "Year 2024 day 18 part 1: " << path.size()-1 << std::endl; // Start is not included in the step count
+
+    std::pair<uint32_t, uint32_t> part2;
+    for (int i = lim; i < bytes.size(); i++) {
+        map.insert(bytes[i]);
+
+        if (path.count(bytes[i]) == 0) { continue; }
+
+        path = find_exit(map, map_size);
+        if (path.size() == 0) {
+            part2 = bytes[i];
+            break;
+        }
+    }
+
+    std::cout << "Year 2024 day 18 part 2: " << part2.first << ',' << part2.second << std::endl;
 }
--- src/y2024/day19.cpp
+++ src/y2024/day19.cpp
@@ -3,6 +3,34 @@
 #include <string>
 #include <sstream>
 #include <cstdint>
+#include <vector>
+#include <unordered_map>
+#include <regex>
+
+uint64_t make_design(std::string design, std::vector<std::string> towels, std::unordered_map<std::string, uint64_t> &cache) {
+    if (design.length() == 0) { return 1; }
+
+    if (cache.count(design) != 0) { return cache[design]; }
+
+    uint64_t num_designs = 0;
+    for (int i = 0; i < towels.size(); i++) {
+        if (towels[i].length() > design.length()) { continue; }
+
+        bool equal = true;
+        for (int j = 0; j < towels[i].length(); j++) {
+            if (towels[i][j] != design[j]) {
+                equal = false;
+                break;
+            }
+        }
+
+        if (equal) { num_designs += make_design(design.substr(towels[i].length()), towels, cache); }
+    }
+
+    cache[design] = num_designs;
+    
+    return num_designs;
+}
 
 void day19(bool test) {
     std::string filepath;
@@ -15,9 +43,35 @@ void day19(bool test) {
         return;
     }
 
+    std::vector<std::string> towels;
+    std::unordered_map<std::string, uint64_t> cache;
+    bool read_towels = true;
+    uint64_t part1 = 0;
+    uint64_t part2 = 0;
+
     std::string line;
     while (std::getline(input, line)) {
-        std::cout << line << std::endl;
+        if (line.length() == 0) {
+            read_towels = false;
+            continue;
+        }
+        if (read_towels) {
+            std::replace(line.begin(), line.end(), ',', ' ');
+            std::stringstream ss(line);
+            std::string towel;
+
+            while (ss >> towel) {
+                towels.push_back(towel);
+            }
+        } else {
+            uint64_t num_designs = make_design(line, towels, cache);
+            if (num_designs > 0) {
+                part1 += 1;
+            }
+            part2 += num_designs;
+        }
     }
-    std::cout << "Year 2024 day 19 not implemented yet" << std::endl;
+
+    std::cout << "Year 2024 day 19 part 1: " << part1 << std::endl;
+    std::cout << "Year 2024 day 19 part 2: " << part2 << std::endl;
 }
