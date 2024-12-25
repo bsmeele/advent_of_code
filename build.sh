6d7bedd33d1d4a6630d2aac5cc0c574a69dc0b3c
bsmeele <bas.smeele2000@gmail.com>
2024-12-25 09:56:08 +0100

    day25

--- build.sh
+++ build.sh
@@ -17,6 +17,23 @@ g++ -std=c++17 \
   "$SRC_DIR/y2024/day06.cpp" \
   "$SRC_DIR/y2024/day07.cpp" \
   "$SRC_DIR/y2024/day08.cpp" \
+  "$SRC_DIR/y2024/day09.cpp" \
+  "$SRC_DIR/y2024/day10.cpp" \
+  "$SRC_DIR/y2024/day11.cpp" \
+  "$SRC_DIR/y2024/day12.cpp" \
+  "$SRC_DIR/y2024/day13.cpp" \
+  "$SRC_DIR/y2024/day14.cpp" \
+  "$SRC_DIR/y2024/day15.cpp" \
+  "$SRC_DIR/y2024/day16.cpp" \
+  "$SRC_DIR/y2024/day17.cpp" \
+  "$SRC_DIR/y2024/day18.cpp" \
+  "$SRC_DIR/y2024/day19.cpp" \
+  "$SRC_DIR/y2024/day20.cpp" \
+  "$SRC_DIR/y2024/day21.cpp" \
+  "$SRC_DIR/y2024/day22.cpp" \
+  "$SRC_DIR/y2024/day23.cpp" \
+  "$SRC_DIR/y2024/day24.cpp" \
+  "$SRC_DIR/y2024/day25.cpp" \
   -o "$OUTPUT"
 
 # Check for success
--- src/y2024/day25.cpp
+++ src/y2024/day25.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <sstream>
 #include <cstdint>
+#include <vector>
+#include <utility>
 
 void day25(bool test) {
     std::string filepath;
@@ -15,9 +17,64 @@ void day25(bool test) {
         return;
     }
 
+    std::vector<std::vector<uint8_t>> locks;
+    std::vector<std::vector<uint8_t>> keys;
+    int flag = 0;
+    std::vector<uint8_t> buf;
+    int y = 0;
+
     std::string line;
     while (std::getline(input, line)) {
-        std::cout << line << std::endl;
+        if (line.length() == 0) {
+            if (flag == -1) {
+                locks.push_back(buf);
+            } else if (flag == 1) {
+                keys.push_back(buf);
+            }
+            buf.clear();
+            flag = 0;
+            y = 0;
+            continue;
+        }
+        if (flag == 0) {
+            if (line[0] == '#') {
+                flag = -1;
+            } else {
+                flag = 1;
+            }
+            for (int i = 0; i < line.length(); i++) {
+                buf.push_back(0);
+            }
+        } else {
+            if (y >= 6) { continue; }
+
+            for (int c = 0; c < line.length(); c++) {
+                if (line[c] == '#') {
+                    buf[c] += 1;
+                }
+            }
+            y += 1;
+        }
     }
-    std::cout << "Year 2024 day 25 not implemented yet" << std::endl;
+    if (flag == -1) {
+        locks.push_back(buf);
+    } else if (flag == 1) {
+        keys.push_back(buf);
+    }
+
+    uint32_t part1 = 0;
+    for (int l = 0; l < locks.size(); l++) {
+        for (int k = 0; k < keys.size(); k++) {
+            bool fits = true;
+            for (int i = 0; i < locks[l].size(); i++) {
+                if (locks[l][i] + keys[k][i] > 6) {
+                    fits = false;
+                    break;
+                }
+            }
+            if (fits) { part1 += 1; }
+        }
+    }
+
+    std::cout << " Year 2024 day 25 part 1: " << part1 << std::endl;
 }
